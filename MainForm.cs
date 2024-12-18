0e9945c0d88a7c602cf0f843edd1dd13f79234c3
jeerawut <jeerawut.jd40@gmail.com>
2024-12-18 14:01:55 +0700

    change theme form

--- MainForm.cs
+++ MainForm.cs
@@ -1,3 +1,5 @@
+using CSharp.WinForms.Material;
+using MaterialSkin;
 using MaterialSkin.Controls;
 
 namespace ExMaterial
@@ -11,6 +13,19 @@ namespace ExMaterial
             this.AutoScaleMode = AutoScaleMode.Dpi;
             this.MinimumSize = new Size(800, 600);
             this.MaximumSize = new Size(800, 600);
+
+            HomeControl homeControl = new HomeControl();
+            homeControl.Dock = DockStyle.Fill;
+            homePage.Controls.Add(homeControl);
+
+            AboutControl aboutControl = new AboutControl();
+            aboutControl.Dock = DockStyle.Fill;
+            aboutPage.Controls.Add(aboutControl);
+
+            var materialSkinManager = MaterialSkinManager.Instance;
+            materialSkinManager.AddFormToManage(this);
+            materialSkinManager.Theme = MaterialSkinManager.Themes.LIGHT;
+            materialSkinManager.ColorScheme = new ColorScheme(Primary.Blue500, Primary.LightBlue900, Primary.BlueGrey500, Accent.LightBlue200, TextShade.WHITE);
         }
     }
 }
