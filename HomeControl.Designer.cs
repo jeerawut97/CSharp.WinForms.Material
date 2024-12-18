384a2dc4f6d83ef1743e7a3f9c1dec793e928542
jeerawut <jeerawut.jd40@gmail.com>
2024-12-18 14:03:30 +0700

    Add user control - home - about

---
+++ HomeControl.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace CSharp.WinForms.Material
+{
+    partial class HomeControl
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            materialLabel1 = new MaterialSkin.Controls.MaterialLabel();
+            SuspendLayout();
+            // 
+            // materialLabel1
+            // 
+            materialLabel1.AutoSize = true;
+            materialLabel1.Depth = 0;
+            materialLabel1.Font = new Font("Roboto", 14F, FontStyle.Regular, GraphicsUnit.Pixel);
+            materialLabel1.ForeColor = SystemColors.ControlText;
+            materialLabel1.Location = new Point(311, 39);
+            materialLabel1.MouseState = MaterialSkin.MouseState.HOVER;
+            materialLabel1.Name = "materialLabel1";
+            materialLabel1.Size = new Size(94, 19);
+            materialLabel1.TabIndex = 0;
+            materialLabel1.Text = "HomeControl";
+            materialLabel1.Click += materialLabel1_Click;
+            // 
+            // HomeControl
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            Controls.Add(materialLabel1);
+            Name = "HomeControl";
+            Size = new Size(800, 600);
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private MaterialSkin.Controls.MaterialLabel materialLabel1;
+    }
+}
---
+++ HomeControl.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CSharp.WinForms.Material
+{
+    public partial class HomeControl : UserControl
+    {
+        public HomeControl()
+        {
+            InitializeComponent();
+        }
+
+        private void materialLabel1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
